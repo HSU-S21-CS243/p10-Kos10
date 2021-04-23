0b552d94ac9e00c24cc31bd0a00577b3dcac6cce
sir-Keatosis <62768320+sir-Keatosis@users.noreply.github.com>
2021-04-23 12:38:12 -0700

    Update XmlCompilationEngine.cpp
    This is difficult and I know that I've made some mistake somewhere I just haven't noticed it yet

--- JackCompiler/XmlCompilationEngine.cpp
+++ JackCompiler/XmlCompilationEngine.cpp
@@ -3,7 +3,31 @@
 void XmlCompilationEngine::compileClass()
 {
 	output << "<class>" << endl;
+	output << _tokens[_tokenCounter]->toString() << endl;
+	_tokenCounter++; //this has to be done after every read of the tokens
 
+	output << _tokens[_tokenCounter]->toString() << endl;
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl;
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	while (_tokens[_tokenCounter]->keyword == Keyword::Static || _tokens[_tokenCounter]->keyword == Keyword::Field)
+	{
+		//todo Zero or More
+		//We have to figure out how to call multiple times for each variable
+		compileClassVarDec();
+	}
+
+	while (_tokens[_tokenCounter]->keyword == Keyword::Constructor
+		|| _tokens[_tokenCounter]->keyword == Keyword::Function
+		|| _tokens[_tokenCounter]->keyword == Keyword::Method
+		)
+	{
+		//todo Zero or More
+		//We have to figure out how to call multiple times for each variable
+		compileSubroutine();
+	}
 
 
 	output << "</class>" << endl;
@@ -12,11 +36,49 @@ void XmlCompilationEngine::compileClassVarDec()
 {
 	output << "<classVarDec>" << endl;
 
+	output << _tokens[_tokenCounter]->toString() << endl; //Var
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Type
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Name
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Semi Colon
+	_tokenCounter++; //this has to be done after every read of the tokens
+
 	output << "</classVarDec>" << endl;
 }
 void XmlCompilationEngine::compileSubroutine()
 {
 	output << "<subroutineDec>" << endl;
+	//Specific code for method //todo make this conditional and add the other types
+	// 
+	//go until the parameter list starts
+	while (_tokens[_tokenCounter]->symbol != Symbol::LeftParentheses)
+	{
+		output << _tokens[_tokenCounter]->toString() << endl; 
+		_tokenCounter++; //this has to be done after every read of the tokens
+	}
+	
+
+	//parameter list
+	compileParameterList();
+
+	output << _tokens[_tokenCounter]->toString() << endl; //for the {
+	_tokenCounter++; //this has to be done after every read of the tokens
+	
+	while (_tokens[_tokenCounter]->keyword == Keyword::Var
+		|| _tokens[_tokenCounter]->keyword == Keyword::Let
+		)
+	{
+		if (_tokens[_tokenCounter]->keyword == Keyword::Var) //not super efficient, but readable
+		{
+			compileVarDec();
+		}
+	}
+
 
 	output << "</subroutineDec>" << endl;
 }
@@ -27,13 +89,37 @@ void XmlCompilationEngine::compileSubroutineCall()
 
 void XmlCompilationEngine::compileParameterList()
 {
+	output << _tokens[_tokenCounter]->toString() << endl; //for the (
+	_tokenCounter++; //this has to be done after every read of the tokens
+
 	output << "<parameterList>" << endl;
+
+	while (_tokens[_tokenCounter]->symbol != Symbol::RightParentheses)
+	{
+		output << _tokens[_tokenCounter]->toString() << endl;
+		_tokenCounter++; //this has to be done after every read of the tokens
+	}
+
+	output << _tokens[_tokenCounter]->toString() << endl; //for the )
+	_tokenCounter++; //this has to be done after every read of the tokens
 
 	output << "</parameterList>" << endl;
 }
 void XmlCompilationEngine::compileVarDec()
 {
 	output << "<varDec>" << endl;
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Var
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Type
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Name
+	_tokenCounter++; //this has to be done after every read of the tokens
+
+	output << _tokens[_tokenCounter]->toString() << endl; //Semi Colon
+	_tokenCounter++; //this has to be done after every read of the tokens
 
 	output << "</varDec>" << endl;
 }
